c54575f592add66502fcbcfcb452026743b1d915
Shubh Goyal <129918065+ShubhG02@users.noreply.github.com>
2024-08-01 23:32:42 -0400

    Add files via upload

---
+++ Includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1dl210/raw"
+processed_folder_path = "/mnt/formula1dl210/processed"
+presentation_folder_path = "/mnt/formula1dl210/presentation"
